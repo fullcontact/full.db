64dfe9440c3135d207e0d1e824ccf9261379ef78
Karlis Lauva <k@karlis.me>
2016-05-31 20:43:00 -0600

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.db "0.9.1-SNAPSHOT"
+(defproject fullcontact/full.db "0.10.0-SNAPSHOT"
   :description "DB sugar (Korma + HarikiCP + core.async)."
   :url "https://github.com/fullcontact/full.db"
   :license {:name "Eclipse Public License - v 1.0"
