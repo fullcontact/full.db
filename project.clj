9708e5f202a5894dbdec2fb1cbd10fd4e53cf0f1
Karlis Lauva <k@karlis.me>
2016-06-21 15:48:12 -0600

    upgrade to 0.10 stack

--- project.clj
+++ project.clj
@@ -5,16 +5,23 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [korma "0.4.2"]
                  [hikari-cp "1.5.0"]
-                 [fullcontact/full.core "0.9.0"]
-                 [fullcontact/full.metrics "0.9.0"]
+                 [fullcontact/full.core "0.10.0"]
+                 [fullcontact/full.metrics "0.10.0"]
                  [fullcontact/full.async "0.9.0"]
                  [org.liquibase/liquibase-core "3.3.5"]
                  [com.mattbertolini/liquibase-slf4j "1.2.1"
-                  :exclusions [org.slf4j/slf4j-api
-                               org.yaml/snakeyaml]]]
+                  :exclusions [org.slf4j/slf4j-api org.yaml/snakeyaml]]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :aot :all
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
