a287b5246c92bb3b00e2a789ddf75a77ff326cc3
Kaspars Dancis <kaspars@fullcontact.com>
2017-04-25 13:15:04 -0600

    Version 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.db "1.0.2-SNAPSHOT"
+(defproject fullcontact/full.db "1.0.2"
   :description "DB sugar (Korma + hikariCP + core.async)."
   :url "https://github.com/fullcontact/full.db"
   :license {:name "Eclipse Public License - v 1.0"
