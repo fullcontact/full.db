778b01a75c1bdba4ad9fc346887f7cb0f98ce4c9
Kaspars Dancis <kaspars@fullcontact.com>
2017-04-25 07:59:24 -0600

    bump versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.db "0.10.4-SNAPSHOT"
+(defproject fullcontact/full.db "1.0.0-SNAPSHOT"
   :description "DB sugar (Korma + hikariCP + core.async)."
   :url "https://github.com/fullcontact/full.db"
   :license {:name "Eclipse Public License - v 1.0"
@@ -6,13 +6,13 @@
             :distribution :repo}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [korma "0.4.2"]
-                 [hikari-cp "1.5.0"]
-                 [fullcontact/full.core "0.10.1" :exclusions [org.clojure/clojurescript]]
+                 [korma "0.4.3"]
+                 [hikari-cp "1.7.5"]
+                 [fullcontact/full.core "1.0.4" :exclusions [org.clojure/clojurescript]]
                  [fullcontact/full.metrics "0.11.4"]
-                 [fullcontact/full.async "0.9.0"]
-                 [org.liquibase/liquibase-core "3.3.5"]
-                 [com.mattbertolini/liquibase-slf4j "1.2.1"
+                 [fullcontact/full.async "1.0.0"]
+                 [org.liquibase/liquibase-core "3.5.3"]
+                 [com.mattbertolini/liquibase-slf4j "2.0.0"
                   :exclusions [org.slf4j/slf4j-api org.yaml/snakeyaml]]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
